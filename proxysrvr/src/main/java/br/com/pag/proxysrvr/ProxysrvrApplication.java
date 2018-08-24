533f6b9bbb20aa2746acd4469cd6e39334272704
Sfalsin <marciosfalsin@gmail.com>
2018-08-24 03:08:00 -0300

    novos arquivos

---
+++ proxysrvr/src/main/java/br/com/pag/proxysrvr/ProxysrvrApplication.java
@@ -0,0 +1,12 @@
+package br.com.pag.proxysrvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProxysrvrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProxysrvrApplication.class, args);
+	}
+}
--- sbac3/src/main/java/br/com/meupag/sbac3/Sbac3Application.java
+++ sbac3/src/main/java/br/com/meupag/sbac3/Sbac3Application.java
@@ -2,7 +2,9 @@ package br.com.meupag.sbac3;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Sbac3Application {
 
